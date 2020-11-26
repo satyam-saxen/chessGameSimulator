0f91d17cffe93f63a26df643d254c10cb7a43060
Satyam Saxena <satyam2saxena@gmail.com>
2020-11-26 11:01:48 +0530

    adding knight class and its moving functionality

--- src/chess_game_simulator/Knight.java
+++ src/chess_game_simulator/Knight.java
@@ -1,2 +1,12 @@
-package chess_game_simulator;public class Knight {
+package chess_game_simulator;
+
+public class Knight extends PieceType{
+    @Override
+    public String PossibleMoves() {
+        System.out.println("Hey I'm here1");
+        moveType = new KnightMove();
+        char row = this.getRow();
+        char column = this.getColumn();
+        return moveType.moves(row,column);
+    }
 }
--- src/chess_game_simulator/KnightMove.java
+++ src/chess_game_simulator/KnightMove.java
@@ -1,2 +1,19 @@
-package chess_game_simulator;public class KnightMove {
+package chess_game_simulator;
+
+public class KnightMove implements MoveType{
+    @Override
+    public String moves(char row, char column) {
+        System.out.println("Hey I'm here");
+        int []X={ 2, 1, -1, -2, -2, -1, 1, 2 };
+        int []Y={ 1, 2, 2, 1, -1, -2, -2, -1 };
+        String possibleValidMoves = "";
+        for(int i=0;i<X.length;i++)
+        {
+            char rowIndex= (char) (row + X[i]);
+            char columnIndex= (char) (column + Y[i]);
+            if(Validation.isPossibleMove(rowIndex,columnIndex))
+                possibleValidMoves += (rowIndex+""+columnIndex+", ");
+        }
+        return possibleValidMoves;
+    }
 }
